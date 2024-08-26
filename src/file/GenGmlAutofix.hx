6f44edfd3d1d753581cbdfe5ddad953e2fa2532e
YellowAfterlife <yellowafterlife@hotmail.com>
2024-08-26 22:13:14 +0300

    Handle a few more auto-conversions from GMS2->GMS1

--- src/file/GenGmlAutofix.hx
+++ src/file/GenGmlAutofix.hx
@@ -8,7 +8,15 @@ using StringTools;
  * @author YellowAfterlife
  */
 class GenGmlAutofix {
+	static function pair(from:String, to:String):GenGmlAutofixPair {
+		return { from: from, to: to };
+	}
+	static var gpu2draw:Array<GenGmlAutofixPair> = [
+		pair("gpu_set_blendmode", "draw_set_blend_mode"),
+		pair("gpu_set_blendmode_ext", "draw_set_blend_mode_ext"),
+	];
 	public static function procGMK(sf:StringWithFlag) {
+		// `var a = argument0, b = argument1` -> `var a, b; a = ...; b = ...`
 		var rsEqu = "\\s*\\:?=\\s*";
 		var rxCommaArg = new EReg("^,\\s*"
 			+ "(\\w+)" + rsEqu + "(argument" + "(?:\\d+|\\[\\d+\\])" + ")"
@@ -34,6 +42,7 @@ class GenGmlAutofix {
 				+ "; " + args.map(a -> a.name + " = " + a.value).join("; ");
 		});
 		
+		// `var a = b` -> `var a; a = b`
 		sf.str = new EReg("(\\b" + "var (\\w+))(" + rsEqu + ")", "g").map(sf.str, function(rx) {
 			var pos = rx.matchedPos().pos;
 			if (pos > 0) {
@@ -47,12 +56,30 @@ class GenGmlAutofix {
 			var snip = rx.matched(1) + "; " + rx.matched(2) + rx.matched(3);
 			return snip;
 		});
+		
+		// `undefined` -> `chr(0)`
+		sf.str = new EReg("\\b" + "undefined" + "\\b", "g").map(sf.str, function(rx) {
+			sf.flag = true;
+			return "chr(0)";
+		});
 	}
 	public static function proc(sf:StringWithFlag) {
 		if (GenGml.version < 1) procGMK(sf);
+		// tabs act weird in GM:S and GM8.1 editors:
 		if (GenGml.version < 2 && sf.str.contains("\t")) {
 			sf.str = sf.str.replace("\t", "    ");
 			sf.flag = true;
 		}
+		// GPU->draw
+		if (GenGml.version < 2) {
+			for (pair in gpu2draw) {
+				var rx = new EReg("\\b" + pair.from + "\\b", "g");
+				sf.str = rx.map(sf.str, function(rx) {
+					sf.flag = true;
+					return pair.to;
+				});
+			}
+		}
 	}
 }
+typedef GenGmlAutofixPair = { from:String, to:String };
